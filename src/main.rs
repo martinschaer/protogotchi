7268e037dbfd092c92ad76566a7265d32686f2f6
Martin Schaer <martin@schaerweb.com>
2023-08-12 12:57:21 +0100

    shows red and blue screens

--- src/main.rs
+++ src/main.rs
@@ -14,34 +14,49 @@ use std::time::Duration;
 
 const SPI_DC: u8 = 9;
 const SPI_CS: u8 = 1;
+const BACKLIGHT: u8 = 13;
 
 fn main() -> ExitCode {
     let gpio = Gpio::new().unwrap();
     let dc = gpio.get(SPI_DC).unwrap().into_output();
     let cs = gpio.get(SPI_CS).unwrap().into_output();
-    let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 60_000_000, Mode::Mode0).unwrap();
-    let di = SPIInterface::new(spi, dc, cs);
+    let mut backlight = gpio.get(BACKLIGHT).unwrap().into_output();
+
     let mut delay = Delay::new();
+
+    let clock_speed = 60_000_000_u32;
+    // let clock_speed = 16_000_000_u32;
+    let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss1, clock_speed, Mode::Mode0).unwrap();
+    let di = SPIInterface::new(spi, dc, cs);
+
     let mut display = Builder::st7789(di)
-        .with_display_size(320, 240)
+        .with_display_size(240, 320)
+        // .with_orientation(mipidsi::Orientation::Landscape(false))
+        .with_invert_colors(mipidsi::ColorInversion::Inverted)
+        // .with_framebuffer_size(width, height)
         .init(&mut delay, None::<OutputPin>)
         .unwrap();
+
+    backlight.set_low();
+    sleep(Duration::from_millis(150));
+    backlight.set_high();
 
     // Clear the display initially
     let style = PrimitiveStyleBuilder::new()
         .fill_color(Rgb565::BLACK)
         .build();
-    Rectangle::new(Point::new(0, 0), Size::new(320, 240))
+    Rectangle::new(Point::new(0, 0), Size::new(240, 320))
         .into_styled(style)
         .draw(&mut display)
         .unwrap();
+    sleep(Duration::from_millis(150));
 
     let red = Rgb565::RED;
 
     loop {
         // Fill the display with red
         let style = PrimitiveStyleBuilder::new().fill_color(red).build();
-        Rectangle::new(Point::new(0, 0), Size::new(320, 240))
+        Rectangle::new(Point::new(0, 0), Size::new(240, 320))
             .into_styled(style)
             .draw(&mut display)
             .unwrap();
@@ -51,9 +66,9 @@ fn main() -> ExitCode {
 
         // Clear the display
         let style = PrimitiveStyleBuilder::new()
-            .fill_color(Rgb565::BLACK)
+            .fill_color(Rgb565::BLUE)
             .build();
-        Rectangle::new(Point::new(0, 0), Size::new(320, 240))
+        Rectangle::new(Point::new(0, 0), Size::new(240, 320))
             .into_styled(style)
             .draw(&mut display)
             .unwrap();
