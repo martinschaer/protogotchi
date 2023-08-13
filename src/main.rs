47b2425e8ba4b7458bcf12420c63feaefaca15c0
Martin Schaer <martin@schaerweb.com>
2023-08-13 15:08:17 +0100

    display text and fps

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use display_interface_spi::SPIInterface;
 use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyle},
     pixelcolor::Rgb565,
     prelude::*,
-    primitives::{PrimitiveStyleBuilder, Rectangle},
+    primitives::Rectangle,
+    text::Text,
 };
+use embedded_graphics_framebuf::FrameBuf;
 use mipidsi::Builder;
 use rppal::gpio::{Gpio, OutputPin};
 use rppal::hal::Delay;
 use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
-use std::process::ExitCode;
+use std::io::Write;
 use std::thread::sleep;
 use std::time::Duration;
+use std::{process::ExitCode, time::Instant};
 
 const SPI_DC: u8 = 9;
 const SPI_CS: u8 = 1;
@@ -28,10 +32,10 @@ fn main() -> ExitCode {
     let spi = Spi::new(Bus::Spi0, SlaveSelect::Ss1, clock_speed, Mode::Mode0).unwrap();
     let di = SPIInterface::new(spi, dc, cs);
 
-    let w = 240_u16;
-    let h = 320_u16;
+    const W: usize = 240;
+    const H: usize = 320;
     let mut display = Builder::st7789(di)
-        .with_display_size(w, h)
+        .with_display_size(W as u16, H as u16)
         // .with_orientation(mipidsi::Orientation::Landscape(false))
         .with_invert_colors(mipidsi::ColorInversion::Inverted)
         .init(&mut delay, None::<OutputPin>)
@@ -43,36 +47,55 @@ fn main() -> ExitCode {
     backlight.set_high();
 
     // Clear the display initially
-    let style = PrimitiveStyleBuilder::new()
-        .fill_color(Rgb565::BLACK)
-        .build();
-    Rectangle::new(Point::new(0, 0), Size::new(w.into(), h.into()))
-        .into_styled(style)
-        .draw(&mut display)
-        .unwrap();
-    sleep(Duration::from_millis(150));
+    display.clear(Rgb565::BLUE).unwrap();
+
+    // Text
+    let character_style = MonoTextStyle::new(&FONT_6X10, Rgb565::new(16, 30, 27));
+
+    // FPS
+    let mut fps = 0_u8;
+    let mut last_time = std::time::Instant::now();
+    let mut now: Instant;
+    let tick = std::time::Duration::from_millis(250);
+    let mut count = 0_u8;
+
+    println!("Starting main loop");
 
     loop {
-        // Fill the display with red
-        let style = PrimitiveStyleBuilder::new().fill_color(Rgb565::RED).build();
-        Rectangle::new(Point::new(0, 0), Size::new(w.into(), h.into()))
-            .into_styled(style)
-            .draw(&mut display)
+        fps += 1;
+        now = std::time::Instant::now();
+        let elapsed = now - last_time;
+
+        if elapsed >= tick {
+            last_time = now;
+            count = (count + 1) % 4;
+            // FPS
+            if count == 0 {
+                print!("\rFPS: {} ", fps);
+                std::io::stdout().flush().unwrap();
+                fps = 0;
+            }
+        }
+
+        // Backend for the buffer
+        let mut data = [Rgb565::new(9, 14, 21); W * H];
+        let mut fbuf = FrameBuf::new(&mut data, W, H);
+
+        // Commodore 64 boot screen
+        let text: String;
+        let prompt =
+            "    **** COMMODORE 64 BASIC V2 ****\n\n 64K RAM SYSTEM  38911 BASIC BYTES FREE\n\nREADY.\n";
+        if count % 2 == 0 {
+            text = format!("{}█", &prompt);
+        } else {
+            text = prompt.to_string();
+        }
+        Text::new(&text, Point::new(5, 5), character_style)
+            .draw(&mut fbuf)
             .unwrap();
 
-        // Wait for some time
-        sleep(Duration::from_millis(500));
-
-        // Clear the display
-        let style = PrimitiveStyleBuilder::new()
-            .fill_color(Rgb565::BLUE)
-            .build();
-        Rectangle::new(Point::new(0, 0), Size::new(w.into(), h.into()))
-            .into_styled(style)
-            .draw(&mut display)
-            .unwrap();
-
-        // Wait for some time
-        sleep(Duration::from_millis(500));
+        // Write it all to the display
+        let area = Rectangle::new(Point::new(0, 0), fbuf.size());
+        display.fill_contiguous(&area, data).unwrap();
     }
 }
